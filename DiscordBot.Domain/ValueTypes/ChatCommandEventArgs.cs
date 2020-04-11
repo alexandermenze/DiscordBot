88ddbda0ccf5f3f217d0ef1dc0fe267a56b1bcdc
Alexander Menze <a.menze@polipol.de>
2020-04-11 15:45:41 +0200

    Added IMessage instance to event args

--- DiscordBot.Domain/ValueTypes/ChatCommandEventArgs.cs
+++ DiscordBot.Domain/ValueTypes/ChatCommandEventArgs.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Discord;
+using System;
 
 namespace DiscordBot.Domain.ValueTypes
 {
     public class ChatCommandEventArgs : EventArgs
     {
+        public IMessage ChatMessage { get; set; }
         public string Command { get; set; }
     }
 }
